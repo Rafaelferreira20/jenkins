ca86a041afbc31a0eb860593a2d008568f21bfda
Rafael Ferreira <33624978+rafaelferreira2@users.noreply.github.com>
2020-03-04 17:25:20 -0400

    123

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,4 +1,4 @@
  apt-get update && \
-  apt-get install -y ruby ruby-dev ruby-bundler build-base libxml2-dev && libxslt-dev postgresql-dev \
+  apt-get install -y ruby ruby-dev ruby-bundler build-essential libxml2-dev && libxslt-dev postgresql-dev \
   curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast && \
   rm -rf /var/lib/apt/lists/*
