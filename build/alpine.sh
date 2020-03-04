0aae470f80a15ff29661d6f8cddab7a72dfa5ccf
Rafael Ferreira <33624978+rafaelferreira2@users.noreply.github.com>
2020-03-04 17:22:25 -0400

    alpine final

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,7 +1,4 @@
-apk update && \
-	   apk add build-base \
-    libxml2-dev \
-    libxslt-dev \
-    postgresql-dev \
-    curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast && \
-    rm -rf /var/cache/apk/*
+ apt-get update && \
+  apt-get install -y ruby ruby-dev ruby-bundler build-base libxml2-dev && libxslt-dev postgresql-dev \
+  curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast && \
+  rm -rf /var/lib/apt/lists/*
