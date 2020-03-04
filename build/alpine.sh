dd3c73305f9837529524dfc19493cb4ee815dca9
Rafael Ferreira <33624978+rafaelferreira2@users.noreply.github.com>
2020-03-04 17:16:52 -0400

    alpine

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,1 +1,4 @@
-docker build
+ apt-get update && \
+  apt-get install -y ruby ruby-dev ruby-bundler build-base libxml2-dev && libxslt-dev postgresql-dev \
+  curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast && \
+  rm -rf /var/lib/apt/lists/*
