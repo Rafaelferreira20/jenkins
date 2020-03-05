af0d7cb66d51b2648a49a5e02eb1ee1444d02129
Rafael Ferreira <33624978+rafaelferreira2@users.noreply.github.com>
2020-03-05 12:10:21 -0400

    Update alpine.sh

--- build/alpine.sh
+++ build/alpine.sh
@@ -1,4 +1,4 @@
-RUN echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && \
+echo "http://dl-4.alpinelinux.org/alpine/v3.9/main" >> /etc/apk/repositories && \
     echo "http://dl-4.alpinelinux.org/alpine/v3.9/community" >> /etc/apk/repositories
 
 apk update && \
